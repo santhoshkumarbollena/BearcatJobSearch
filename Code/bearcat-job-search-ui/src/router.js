da9543e92976a56462d5a88864adbaf7ad43da55
Anil Bomma <s537161@nwmissouri.edu>
2020-06-22 23:21:39 -0500

    added login page in UI

--- Code/bearcat-job-search-ui/src/router.js
+++ Code/bearcat-job-search-ui/src/router.js
@@ -1,17 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/login",
+      name: "login",
+      component: Login,
     },
     {
       path: "/about",
@@ -20,7 +19,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+    },
+  ],
 });
---
+++ Code/bearcat-job-search-ui/src/views/Login.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <div class="sidenav">
+      <div class="login-main-text">
+        <h2>
+          Bearcat Job Search
+          <br />Login Page
+        </h2>
+        <p>Login or register from here to access.</p>
+      </div>
+    </div>
+    <div class="main">
+      <div class="col-md-6 col-sm-12">
+        <div class="login-form border p-3">
+          <form>
+            <div class="form-group">
+              <label>User Name</label>
+              <input type="text" class="form-control" placeholder="User Name" />
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control" placeholder="Password" />
+            </div>
+            <button type="submit" class="btn btn-black mr-2">Login</button>
+            <button type="submit" class="btn btn-secondary">Register</button>
+
+            <div class="form-group mt-3">
+              <a href="/#/login">Forget password</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style scoped>
+.nw-bg {
+  background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
+  background-color: #cccccc;
+  background-repeat: no-repeat;
+}
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.main-head {
+  height: 150px;
+  background: #fff;
+}
+
+.sidenav {
+  height: 100%;
+  background-color: #000;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.main {
+  padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {
+    padding-top: 15px;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .login-form {
+    margin-top: 10%;
+  }
+
+  .register-form {
+    margin-top: 10%;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .main {
+    margin-left: 40%;
+  }
+
+  .sidenav {
+    width: 40%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+  }
+
+  .login-form {
+    margin-top: 50%;
+  }
+
+  .register-form {
+    margin-top: 20%;
+  }
+}
+
+.login-main-text {
+  margin-top: 20%;
+  padding: 60px;
+  color: #fff;
+}
+
+.login-main-text h2 {
+  font-weight: 300;
+}
+
+.btn-black {
+  background-color: #000 !important;
+  color: #fff;
+}
+</style>
