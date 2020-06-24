623554998c414229953ca71d5e9f1408960c39aa
yugimamidi <yugi.mamidi01@gmail.com>
2020-06-23 22:59:26 -0500

    - updated login page

--- Code/bearcat-job-search-ui/src/views/Login.vue
+++ Code/bearcat-job-search-ui/src/views/Login.vue
@@ -6,18 +6,18 @@
           Bearcat Job Search
           <br />Login Page
         </h2>
-        <p>Login or register from here to access.</p>
+        <p>Login from here to access the application.</p>
       </div>
     </div>
     <div class="main">
       <div class="col-md-6 col-sm-12">
         <div class="login-form border p-3">
           <form>
-            <div class="form-group">
+            <div class="form-group text-left">
               <label>User Name</label>
               <input type="text" class="form-control" placeholder="User Name" />
             </div>
-            <div class="form-group">
+            <div class="form-group text-left">
               <label>Password</label>
               <input type="password" class="form-control" placeholder="Password" />
             </div>
@@ -40,82 +40,3 @@ export default {
 };
 </script>
 
-<style scoped>
-.nw-bg {
-  background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
-  background-color: #cccccc;
-  background-repeat: no-repeat;
-}
-body {
-  font-family: "Lato", sans-serif;
-}
-
-.main-head {
-  height: 150px;
-  background: #fff;
-}
-
-.sidenav {
-  height: 100%;
-  background-color: #000;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.main {
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {
-    padding-top: 15px;
-  }
-}
-
-@media screen and (max-width: 450px) {
-  .login-form {
-    margin-top: 10%;
-  }
-
-  .register-form {
-    margin-top: 10%;
-  }
-}
-
-@media screen and (min-width: 768px) {
-  .main {
-    margin-left: 40%;
-  }
-
-  .sidenav {
-    width: 40%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-  }
-
-  .login-form {
-    margin-top: 50%;
-  }
-
-  .register-form {
-    margin-top: 20%;
-  }
-}
-
-.login-main-text {
-  margin-top: 20%;
-  padding: 60px;
-  color: #fff;
-}
-
-.login-main-text h2 {
-  font-weight: 300;
-}
-
-.btn-black {
-  background-color: #000 !important;
-  color: #fff;
-}
-</style>
