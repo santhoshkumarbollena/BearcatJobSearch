b2324080c8749ea1eb9fbb79330a69db0d16314c
Anil Bomma <s537161@nwmissouri.edu>
2020-06-23 20:09:21 -0500

    bug fixed in student model

--- Code/BearcatJobSearchAPIS/app/Models/Student.js
+++ Code/BearcatJobSearchAPIS/app/Models/Student.js
@@ -1,9 +1,32 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Student extends Model {
+  static get primaryKey() {
+    return "studentId";
+  }
+
+  intrestedPositions() {
+    return this.hasMany("App/Models/IntrestedPosition");
+  }
+
+  proficientCourses() {
+    return this.hasMany("App/Models/ProficientCourse");
+  }
+
+  studentApplications() {
+    return this.belongsToMany(
+      "App/Models/Job",
+      "studentId",
+      "jobId",
+      "studentId",
+      "id"
+    )
+      .pivotTable("student_applications")
+      .withPivot("status");
+  }
 }
 
-module.exports = Student
+module.exports = Student;
