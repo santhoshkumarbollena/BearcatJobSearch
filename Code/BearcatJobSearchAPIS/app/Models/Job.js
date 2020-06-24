b91e7a9a9a9eb731a0a12b83d168e7f7188bb27f
Anil Bomma <s537161@nwmissouri.edu>
2020-06-23 20:08:59 -0500

    - bug fixed in job model

--- Code/BearcatJobSearchAPIS/app/Models/Job.js
+++ Code/BearcatJobSearchAPIS/app/Models/Job.js
@@ -1,9 +1,28 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Job extends Model {
+  static get primaryKey() {
+    return "id";
+  }
+
+  createdBy() {
+    return this.hasOne("App/Models/User");
+  }
+
+  jobApplications() {
+    return this.belongsToMany(
+      "App/Models/Student",
+      "jobId",
+      "studentId",
+      "id",
+      "studentId"
+    )
+      .pivotTable("student_applications")
+      .withPivot("status");
+  }
 }
 
-module.exports = Job
+module.exports = Job;
