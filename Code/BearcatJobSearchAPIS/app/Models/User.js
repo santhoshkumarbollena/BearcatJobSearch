8d66d1102cfefc19fca0969bf597585f7d8aee8a
Anil Bomma <s537161@nwmissouri.edu>
2020-06-23 20:09:08 -0500

    - bug fixed in user model

--- Code/BearcatJobSearchAPIS/app/Models/User.js
+++ Code/BearcatJobSearchAPIS/app/Models/User.js
@@ -1,9 +1,16 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class User extends Model {
+  static get primaryKey() {
+    return "userId";
+  }
+
+  createdJobs() {
+    return this.belongsToMany("App/Models/Job");
+  }
 }
 
-module.exports = User
+module.exports = User;
