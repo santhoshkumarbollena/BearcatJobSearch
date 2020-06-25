942c4170b2fa38afd9682e8757b990ea9c5b4263
yugimamidi <yugi.mamidi01@gmail.com>
2020-06-24 22:39:46 -0500

    Added css for the registration form

--- Code/bearcat-job-search-ui/src/views/Registration.vue
+++ Code/bearcat-job-search-ui/src/views/Registration.vue
@@ -54,3 +54,86 @@ export default {
   name: "Registration"
 };
 </script>
+
+<style scoped>
+.nw-bg {
+  background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
+  background-color: #cccccc;
+  background-repeat: no-repeat;
+}
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.main-head {
+  height: 150px;
+  background: #fff;
+}
+
+.sidenav {
+  height: 100%;
+  background-color: #000;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.main {
+  padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {
+    padding-top: 15px;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .Registration-form {
+    margin-top: 10%;
+  }
+
+  .register-form {
+    margin-top: 10%;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .main {
+    margin-left: 40%;
+  }
+
+  .sidenav {
+    width: 40%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+  }
+
+  .Registration-form {
+    margin-top: 50%;
+  }
+
+
+}
+
+.Registration-main-text {
+  margin-top: 20%;
+  padding: 60px;
+  color: #fff;
+}
+
+.Registration-main-text h2 {
+  font-weight: 300;
+}
+
+.btn-black {
+  background-color: #000 !important;
+  color: #fff;
+}
+
+.btn:hover {
+  color: #aaa
+}
+
+</style>
