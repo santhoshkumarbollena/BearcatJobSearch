3745299751e1f94808bc79bed6bfce3470da239e
yugimamidi <yugi.mamidi01@gmail.com>
2020-06-25 21:22:30 -0500

    updated with font awesome icons

--- Code/bearcat-job-search-ui/src/views/Registration.vue
+++ Code/bearcat-job-search-ui/src/views/Registration.vue
@@ -1,65 +1,95 @@
 <template>
-  <div>
-    <div class="sidenav">
-      <div class="Registration-main-text">
-        <h2>
+	<div>
+		<div class="sidenav">
+			<div class="Registration-main-text">
+				<h2>
           Bearcat Job Search
           <br />Registration Page
         </h2>
-        <p>Registration to access the application.</p>
-      </div>
-    </div>
-    <div class="main form-outline">
-      <div class="col-md-6">
-        <div class="register-form p-3">
-          <form>
-           <div class="form-group text-left">
-              <label><b>First Name</b></label>
-              <input type="text" class="form-control" placeholder="Firstname" />
-            </div>
-            <div class="form-group text-left">
-              <label><b>Last Name</b></label>
-              <input type="text" class="form-control" placeholder="Lastname" />
-            </div>
-             <div class="form-group text-left">
-              <label><b>User Name</b></label>
-              <input type="text" class="form-control" placeholder="User Name" />
-            </div>
-            <div class="form-group text-left">
-              <label><b>Password</b></label>
-              <input type="password" class="form-control" placeholder="New Password" />
-            </div>
-             <div class="form-group text-left">
-            <label><b>Gender</b></label>
-                  <div class="maxl">
-                    <label class="radio inline">
-                      <input type="radio" name="gender" value="male" checked />
-                      <span> Male</span>
-                    </label>&nbsp&nbsp
-                    <label class="radio inline">
-                      <input type="radio" name="gender" value="female" />
-                      <span> Female</span>
-                    </label>
-                  </div>
-            </div>
-            <div class="form-group text-left">
-                  <label><b>Phone Number</b></label>
-                  <input
-                    type="text"
-                    minlength="10"
-                    maxlength="10"
-                    name="txtEmpPhone"
-                    class="form-control"
-                    placeholder="Your Phone"
-                    value
-                  />
-              </div>
-              <input type="submit" class="btn btn-primary" value="Register" />
-             </form>
-        </div>
-      </div>
-    </div>
-  </div>
+				<p>Registration to access the application.</p>
+			</div>
+		</div>
+		<div class="main form-outline">
+			<section>
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-12 col-md-8 col-lg-8 col-xl-6">
+							<div class="row align-items-center mt-4">
+								<div class="col">
+									<div class="input-group"> <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                 <i class="fa fa-user fa-1x"></i>
+                 </span>
+										<input type="text" class="form-control" placeholder="First Name"> </div>
+								</div>
+							</div>
+							<div class="row align-items-center mt-4">
+								<div class="col">
+									<div class="input-group"> <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                 <i class="fa fa-user fa-1x"></i>
+                 </span>
+										<input type="text" class="form-control" placeholder="Last Name"> </div>
+								</div>
+							</div>
+							<div class="row align-items-center mt-4">
+								<div class="col">
+									<div class="input-group"> <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                 <i class="fa fa-phone fa-1x"></i>
+                 </span>
+										<input type="text" class="form-control" placeholder="Phone Number"> </div>
+								</div>
+							</div>
+							<div class="row text-left mt-4">
+								<div class="col">
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Male">
+										<label class="form-check-label" for="inlineRadio1">Male</label> <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-1x"></i>
+                  <i class="fa fa-male fa-stack-1x fa-inverse"></i>
+                </span> </div>
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Female">
+										<label class="form-check-label" for="inlineRadio2">Female</label> <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-1x"></i>
+                  <i class="fa fa-female fa-stack-1x fa-inverse"></i>
+                </span> </div>
+								</div>
+							</div>
+							<div class="row align-items-center mt-4">
+								<div class="col">
+									<div class="input-group"> <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                 <i class="fa fa-envelope fa-1x"></i>
+                 </span>
+										<input type="email" class="form-control" placeholder="Email"> </div>
+								</div>
+							</div>
+							<div class="row align-items-center mt-4">
+								<div class="col">
+									<div class="input-group"> <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                 <i class="fa fa-lock fa-1x"></i>
+                 </span>
+										<input type="password" class="form-control" placeholder="Password"> </div>
+								</div>
+							</div>
+							<div class="row text-left mt-4">
+								<div class="col">
+									<div class="form-check">
+										<label class="form-check-label">
+											<input type="checkbox" class="form-check-input"> I Read and Accept <a href="#">Terms and Conditions</a> </label>
+									</div>
+									<button class="btn btn-primary mt-4">Register</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -67,7 +97,6 @@ export default {
   name: "Registration"
 };
 </script>
-
 <style scoped>
 .nw-bg {
   background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
