a704f7ba93dfabbd74d96c07fcff7fcd69ac7d12
MahenderReddy Surkanti <54380574+Mahender1166@users.noreply.github.com>
2020-06-29 11:23:40 -0500

    Create ForgotPassword.vue

---
+++ Code/bearcat-job-search-ui/src/views/ForgotPassword.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Forgot password page</div>
+</template>
+
+<script>
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
