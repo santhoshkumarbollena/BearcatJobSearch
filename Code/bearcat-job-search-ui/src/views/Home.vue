cedad58468651d764031255ef723e7583900c647
prasu93 <prasannamahesh93@gmail.com>
2020-07-01 18:37:47 -0500

    Inserted changepassword button in homepage

--- Code/bearcat-job-search-ui/src/views/Home.vue
+++ Code/bearcat-job-search-ui/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Welcome to home page</div>
+  <div>Welcome to home page
+  <div>
+    <router-link to="/change-password" class="border">
+                  <button class="btn btn-primary mt-4 pull-right">Change Password</button>
+                  </router-link>
+  </div>
+  </div>
+  
 </template>
 
 <script>
