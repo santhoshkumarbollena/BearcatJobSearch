e5749582de7c9bb7a9b5caa1db099741b3fc3e48
yugimamidi <yugi.mamidi01@gmail.com>
2020-07-06 15:14:41 -0500

    added background functionality for the reset logic

--- Code/bearcat-job-search-ui/src/views/ResetPassword.vue
+++ Code/bearcat-job-search-ui/src/views/ResetPassword.vue
@@ -1,92 +1,33 @@
 <template>
-  <div>
-    <div class="sidenav">
-      <div class="ForgotPassword-main-text">
-        <h2>
-          Bearcat Job Search
-          <br />Reset Password Page
-        </h2>
-        <p>Reset Password to access the application</p>
-      </div>
-    </div>
-    <div class="main form-outline">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-12 col-md-8 col-lg-8 col-xl-6">
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <i class="fa fa-unlock fa-4x"></i>
-              </div>
-            </div>
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <h2>Reset Password</h2>
-              </div>
-            </div>
-            <div v-if="!error">
-              <div class="row align-items-center mt-4">
-                <div class="col">
-                  <div class="input-group">
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-key fa-1x"></i>
-                    </span>
-                    <input
-                      type="password"
-                      class="form-control"
-                      placeholder="New Password"
-                      v-model="secret.password"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="row align-items-center mt-4">
-                <div class="col">
-                  <div class="input-group">
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-key fa-1x"></i>
-                    </span>
-                    <input
-                      type="password"
-                      class="form-control"
-                      placeholder="Confirm New Password"
-                      v-model="secret.confirmPassword"
-                    />
-                    <small class="text-left text-danger col-sm-12 ml-3">{{passError}}</small>
-                  </div>
-                </div>
-              </div>
-              <div class="row align-items-center mt-4">
-                <div class="col">
-                  <input
-                    class="btn btn-primary btn-block"
-                    value="Reset Password"
-                    type="submit"
-                    @click.prevent.stop="changePassword"
-                  />
-                </div>
-              </div>
-            </div>
-             <div class="row align-items-center mt-4">
-                <div class="col">
-                <router-link to="/login">
-                  <input
-                    class="btn btn-primary btn-block"
-                    value="Click here to Login"
-                  />
-                </router-link>
-                </div>
-            </div>
-            <div class="row align-items-center mt-4">
-              <div class="col-sm-12">
-                <Loader v-if="loader"></Loader>
-                <b-alert show variant="danger" v-if="error">{{error}}</b-alert>
-                <b-alert show variant="success" v-if="success">{{success}}</b-alert>
-              </div>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 v-center p-4 border-2px">
+        <h1 class="text-center" style="font-family:argon">RESET PASSWORD</h1>
+        <div class="offset-md-5">
+         <i class="fa fa-unlock fa-3x"></i>
         </div>
+         <base-input
+                    type="password"
+                    placeholder="New Password"
+                    addon-left-icon="fa fa-key"
+                    v-model="secret.password"
+                    required
+        ></base-input>
+        <base-input
+                    type="password"
+                    placeholder="Confirm New Password"
+                    addon-left-icon="fa fa-key"
+                    v-model="secret.confirmPassword"
+                    required
+        ></base-input>
+        <small class="text-left text-danger col-sm-12 ml-3">{{passError}}</small>
+         <base-button block type="primary" class="mb-2" icon="fa fa-unlock" @click.prevent.stop="changePassword">Reset Password</base-button>
+         <router-link to="/login">
+              <base-button block type="primary"  icon="fa fa-sign-in" >Click here to Login</base-button>
+         </router-link>
+         <Loader v-if="loader"></Loader>
+          <div class="alert alert-danger" role="alert" v-if="error">{{error}}</div>
+          <div class="alert alert-success" role="alert" v-if="success">{{success}}</div>
       </div>
     </div>
   </div>
@@ -149,84 +90,14 @@ export default {
 };
 </script>
 
-
 <style scoped>
-.nw-bg {
-  background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
-  background-color: #cccccc;
-  background-repeat: no-repeat;
-}
-body {
-  font-family: "Lato", sans-serif;
+.v-center {
+  margin-top: 10%;
 }
 
-.main-head {
-  height: 150px;
-  background: #fff;
-}
-
-.sidenav {
-  height: 100%;
-  background-color: #000;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.main {
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {
-    padding-top: 15px;
-  }
-}
-
-@media screen and (max-width: 450px) {
-  .ForgotPassword-form {
-    margin-top: 10%;
-  }
-
-  .register-form {
-    margin-top: 10%;
-  }
-}
-
-@media screen and (min-width: 768px) {
-  .main {
-    margin-left: 40%;
-  }
-
-  .sidenav {
-    width: 40%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-  }
-
-  .ForgotPassword-form {
-    margin-top: 50%;
-  }
-}
-
-.ForgotPassword-main-text {
-  margin-top: 20%;
-  padding: 60px;
-  color: #fff;
-}
-
-.ForgotPassword-main-text h2 {
-  font-weight: 300;
-}
-
-.btn-black {
-  background-color: #000 !important;
-  color: #fff;
-}
-
-.btn:hover {
-  color: #aaa;
+.border-2px {
+  border: 1px solid #999;
+  border-radius: 20px;
+  background-color: #fff;
 }
 </style>
-
