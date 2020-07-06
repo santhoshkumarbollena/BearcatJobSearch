64e09095a93bc240c174c68b8d660bdd52636b2c
yugimamidi <yugi.mamidi01@gmail.com>
2020-07-06 15:14:07 -0500

    added bacground image for the respective pages

--- Code/bearcat-job-search-ui/src/views/ChangePassword.vue
+++ Code/bearcat-job-search-ui/src/views/ChangePassword.vue
@@ -1,54 +1,29 @@
 <template>
-	<div>
-		<div class="main form-outline">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-md-4">
-						<div class="row align-items-center mt-4">
-							<div class="col"> <i class="fa fa-lock fa-4x"></i> </div>
-						</div>
-						<div class="row align-items-center mt-4">
-							<div class="col">
-								<h2>Change Password</h2> </div>
-						</div>
-                        <div class="row align-items-center mt-4">
-							<div class="col">
-								<div class="input-group"> <span class="fa-stack fa-lg">
-                                <i class="fa fa-square-o fa-stack-2x"></i>
-                                <i class="fa fa-key fa-1x"></i>
-                                </span>
-								<input type="password" class="form-control" placeholder="Old Password" /> </div>
-							</div>
-						</div>
-						<div class="row align-items-center mt-4">
-							<div class="col">
-								<div class="input-group"> <span class="fa-stack fa-lg">
-                                <i class="fa fa-square-o fa-stack-2x"></i>
-                                <i class="fa fa-key fa-1x"></i>
-                                </span>
-								<input type="password" class="form-control" placeholder="New Password" /> </div>
-							</div>
-						</div>
-                        <div class="row align-items-center mt-4">
-							<div class="col">
-								<div class="input-group"> <span class="fa-stack fa-lg">
-                                <i class="fa fa-square-o fa-stack-2x"></i>
-                                <i class="fa fa-key fa-1x"></i>
-                                </span>
-								<input type="password" class="form-control" placeholder="Confirm New Password" /> </div>
-							</div>
-						</div>
-                           <div class="row align-items-center mt-4">
-							<div class="col">
-								<input class="btn btn-primary btn-block text-center" value="Save" type="submit">
-							</div>
-                            <div class="col">
-								<input class="btn btn-primary btn-block text-center" value="Cancel" type="submit">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 v-center p-4 border-2px">
+        <h1 class="text-center" style="font-family:argon">CHANGE PASSWORD</h1>
+		<div class="offset-md-5">
+         <i class="fa fa-lock fa-3x"></i>
+        </div>
+		<base-input
+			type="password"
+			placeholder="Old Password"						
+			addon-left-icon="fa fa-key"
+		></base-input>
+		<base-input
+			type="password"
+			placeholder="New Password"
+			addon-left-icon="fa fa-unlock"
+		></base-input>
+		<base-input
+			type="password"
+			placeholder="Confirm New Password"
+			addon-left-icon="fa fa-unlock"
+		></base-input>
+		<base-button type="primary pull-left" icon="fa fa-save" >Save</base-button>
+		<base-button type="primary pull-right" icon="fa fa-times" >Cancel</base-button>
+      </div>
+    </div>
 	</div>
 </template>
--- Code/bearcat-job-search-ui/src/views/ForgotPassword.vue
+++ Code/bearcat-job-search-ui/src/views/ForgotPassword.vue
@@ -1,57 +1,20 @@
 <template>
-  <div>
-    <div class="sidenav">
-      <div class="ForgotPassword-main-text">
-        <h2>
-          Bearcat Job Search
-          <br />Forgot Password Page
-        </h2>
-        <p>Forgot Password to access the application</p>
-      </div>
-    </div>
-    <div class="main form-outline">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-12 col-md-8 col-lg-8 col-xl-6">
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <i class="fa fa-lock fa-4x"></i>
-              </div>
-            </div>
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <h2>Forgot Password?</h2>
-                <p>Please enter your registered email, a verification link will be sent to your email</p>
-              </div>
-            </div>
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <div class="input-group">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-envelope fa-1x"></i>
-                  </span>
-                  <input type="email" class="form-control" placeholder="Email" v-model="email" />
-                </div>
-              </div>
-            </div>
-            <div class="row align-items-center mt-4">
-              <div class="col">
-                <input
-                  class="btn btn-primary btn-block"
-                  value="Send Link"
-                  type="submit"
-                  @click.prevent="sendLink"
-                />
-              </div>
-              <div class="col-sm-12 mt-3">
-                <Loader v-if="loader" class="mt-4"></Loader>
-                <b-alert show variant="danger" v-if="error">{{error}}</b-alert>
-                <b-alert show variant="success" v-if="success">{{success}}</b-alert>
-              </div>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 v-center p-4 border-2px">
+        <h1 class="text-center" style="font-family:argon">FORGOT PASSWORD?</h1>
+        <div class="offset-md-5">
+         <i class="fa fa-lock fa-3x"></i>
         </div>
+        <p>Please enter your registered email,</p>
+         <base-input
+                placeholder="Enter your username or email id"  type="email" addon-left-icon="fa fa-user"
+                v-model="email"
+         ></base-input>
+         <base-button block type="primary"  icon="fa fa-link" @click.prevent="sendLink">Send Link</base-button>
+         <Loader v-if="loader" class="mt-4"></Loader>
+         <div class="alert alert-danger" role="alert" v-if="error">{{error}}</div>
+         <div class="alert alert-success" role="alert" v-if="success">{{success}}</div>
       </div>
     </div>
   </div>
@@ -98,82 +61,14 @@ export default {
 </script>
 
 <style scoped>
-.nw-bg {
-  background-image: url("https://lh5.googleusercontent.com/p/AF1QipP2PpiWMDm_uvLraBe3Esmow4Wki2lwkNF-2S1k=w203-h318-k-no");
-  background-color: #cccccc;
-  background-repeat: no-repeat;
-}
-body {
-  font-family: "Lato", sans-serif;
+.v-center {
+  margin-top: 10%;
 }
 
-.main-head {
-  height: 150px;
-  background: #fff;
-}
-
-.sidenav {
-  height: 100%;
-  background-color: #000;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.main {
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {
-    padding-top: 15px;
-  }
-}
-
-@media screen and (max-width: 450px) {
-  .ForgotPassword-form {
-    margin-top: 10%;
-  }
-
-  .register-form {
-    margin-top: 10%;
-  }
-}
-
-@media screen and (min-width: 768px) {
-  .main {
-    margin-left: 40%;
-  }
-
-  .sidenav {
-    width: 40%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-  }
-
-  .ForgotPassword-form {
-    margin-top: 50%;
-  }
-}
-
-.ForgotPassword-main-text {
-  margin-top: 20%;
-  padding: 60px;
-  color: #fff;
-}
-
-.ForgotPassword-main-text h2 {
-  font-weight: 300;
-}
-
-.btn-black {
-  background-color: #000 !important;
-  color: #fff;
-}
-
-.btn:hover {
-  color: #aaa;
+.border-2px {
+  border: 1px solid #999;
+  border-radius: 20px;
+  background-color: #fff;
 }
 </style>
 
