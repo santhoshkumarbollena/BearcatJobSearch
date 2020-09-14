223ecaa35acfe6cf5df811e0cde4f73c71bcb322
MahenderReddy Surkanti <54380574+Mahender1166@users.noreply.github.com>
2020-09-13 21:39:43 -0500

    Udating STudent application controller

---
+++ Code/BearcatJobSearchAPIS/app/Controllers/Http/StudentApplicationController.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const _ = use("lodash");
+const Hash = use("Hash");
+const Student = use("App/Models/Student");
+const Job = use("App/Models/Job")
+
+class StudentApplicationController {
+
+    
+   
+
+}
+module.exports = StudentApplicationController;
