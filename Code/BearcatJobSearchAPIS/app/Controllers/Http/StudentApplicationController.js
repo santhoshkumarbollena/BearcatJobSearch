2b38dea7587e2a280abf03a2749d65a016528a78
MahenderReddy Surkanti <54380574+Mahender1166@users.noreply.github.com>
2020-09-13 21:40:21 -0500

    API for students who are applied for the job

--- Code/BearcatJobSearchAPIS/app/Controllers/Http/StudentApplicationController.js
+++ Code/BearcatJobSearchAPIS/app/Controllers/Http/StudentApplicationController.js
@@ -7,7 +7,16 @@ const Job = use("App/Models/Job")
 
 class StudentApplicationController {
 
-    
+    async StudentApplyJob({ request, auth, response, params }) {
+        console.log(params.studentId,params.jobId);
+        const job = await Job.find(params.jobId);
+        const student = await Student.find(params.studentId);
+
+        student.studentApplications().attach([job.id], (studentApplication) => {
+            studentApplication.status = "Applied";
+          });
+  
+    }
    
 
 }
