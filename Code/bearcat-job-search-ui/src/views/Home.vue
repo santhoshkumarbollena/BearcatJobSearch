cdf953a1735e790306da81f095a79f274edacafd
Anil Bomma <anil.bommaa@gmail.com>
2020-09-14 01:24:04 -0500

    added signout code in home.vue

--- Code/bearcat-job-search-ui/src/views/Home.vue
+++ Code/bearcat-job-search-ui/src/views/Home.vue
@@ -1,54 +1,96 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-default">
     <div class="container">
-        <a class="navbar-brand nav-link nav-link-icon" href="#"><i class="fa fa-home"></i> Home</a>
-        <a class="navbar-brand nav-link nav-link-icon" href="#"><i class="fa fa-book"></i> Careers</a>
-        <a class="navbar-brand nav-link nav-link-icon" href="#"><i class="fa fa-phone-square"></i> About us</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-default">
-            <div class="navbar-collapse-header">
-                <div class="row">
-                    <div class="col-6 collapse-brand"> 
-                        <a href="#">
-                            <img src="assets/img/brand/blue.png">
-                        </a>
-                    </div>
-                    <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
-                            <span></span>
-                            <span></span>
-                        </button>
-                    </div>
-                </div>
+      <a class="navbar-brand nav-link nav-link-icon" href="#">
+        <i class="fa fa-home"></i> Home
+      </a>
+      <a class="navbar-brand nav-link nav-link-icon" href="#">
+        <i class="fa fa-book"></i> Careers
+      </a>
+      <a class="navbar-brand nav-link nav-link-icon" href="#">
+        <i class="fa fa-phone-square"></i> About us
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar-default"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar-default">
+        <div class="navbar-collapse-header">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a href="#">
+                <img src="assets/img/brand/blue.png" />
+              </a>
             </div>
-            
-            <ul class="navbar-nav ml-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="#">
-                        <i class="fa fa-sign-out"></i> sign-out
-                    </a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link nav-link-icon" href="#" id="navbar-default_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="ni ni-settings-gear-65"></i> settings
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
-                       <router-link to="/change-password">
-                        <a class="dropdown-item">Change Password</a>
-                       </router-link>
-                    </div>
-                </li>
-            </ul>
+            <div class="col-6 collapse-close">
+              <button
+                type="button"
+                class="navbar-toggler"
+                data-toggle="collapse"
+                data-target="#navbar-default"
+                aria-controls="navbar-default"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
         </div>
+
+        <ul class="navbar-nav ml-lg-auto">
+          <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#" @click="logout">
+              <i class="fa fa-sign-out"></i> sign-out
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link nav-link-icon"
+              href="#"
+              id="navbar-default_dropdown_1"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i class="ni ni-settings-gear-65"></i> settings
+            </a>
+            <div
+              class="dropdown-menu dropdown-menu-right"
+              aria-labelledby="navbar-default_dropdown_1"
+            >
+              <router-link to="/change-password">
+                <a class="dropdown-item">Change Password</a>
+              </router-link>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push({ name: "login" });
+    }
+  }
 };
 </script>
 
