35f2602324f7aa548e58f54b0b4139c57caf7999
Anil Bomma <anil.bommaa@gmail.com>
2020-09-14 09:55:41 -0500

    working on chat functionality

--- Code/BearcatJobSearchAPIS/app/Controllers/Http/JobController.js
+++ Code/BearcatJobSearchAPIS/app/Controllers/Http/JobController.js
@@ -3,46 +3,37 @@
 const _ = use("lodash");
 const Hash = use("Hash");
 const Student = use("App/Models/Student");
-const Job = use("App/Models/Job")
+const Job = use("App/Models/Job");
 
 class JobController {
+  async getAllJobs({ request, auth, response, params }) {
+    const job = await Job.all();
+    return response.status(200).json(job);
+  }
 
-    async getAllJobs({ request, auth, response, params }) {
-        const job = await Job.all();
-        console.log(job)
-        return response.status(200).json(job);
+  async getJobBasedOnId({ request, auth, response, params }) {
+    const job = await Job.find(params.jobId);
+    return response.status(200).json(job);
+  }
 
-    }
-    async getJobBasedOnId({ request, auth, response, params }) {
-        //console.log(params.jobId)
-        const job = await Job.find(params.jobId);
-        //console.log(job)
-        return response.status(200).json(job);
-    }
+  async createJob({ request, auth, response, params }) {
+    let jobInput = request.all();
+    const job = await Job.findOrCreate(jobInput);
+    return response.status(200).json(job);
+  }
 
-    async createJob({ request, auth, response, params }) {
-        let jobInput = request.all();
-        console.log(jobInput)
-        const job = await Job.findOrCreate(jobInput);
-        return response.status(200).json(job);
-    }
+  async updateJob({ request, auth, response, params }) {
+    let jobInput = request.all();
+    const job = await Job.find(params.id);
+    job = _.merge(job, jobInput);
+    await job.save();
+    return response.status(200).json(job);
+  }
 
-    async updateJob({ request, auth, response, params }) {
-        let jobInput = request.all();
-        console.log(jobInput)
-        const job = await Job.find(params.id);
-        job = _.merge(job, jobInput);
-        await job.save();
-        return response.status(200).json(job);
-    }
-
-    async deleteJob({ request, auth, response, params }) {
-
-        const job = await Job.find(params.id);
-        await job.delete();
-        return response.status(200).json(job);
-    }
-   
-
+  async deleteJob({ request, auth, response, params }) {
+    const job = await Job.find(params.id);
+    await job.delete();
+    return response.status(200).json(job);
+  }
 }
 module.exports = JobController;
--- Code/bearcat-job-search-ui/src/App.vue
+++ Code/bearcat-job-search-ui/src/App.vue
@@ -8,7 +8,38 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {};
+  },
+  created() {
+    console.log("----- from app.js");
+    const ws = adonis.Ws("ws://localhost:3006");
+    ws.connect();
+
+    let isConnected = false;
+
+    ws.on("open", () => {
+      isConnected = true;
+    });
+
+    ws.on("close", () => {
+      isConnected = false;
+    });
+
+    const chat = ws.subscribe("chat");
+
+    chat.on("ready", () => {
+      chat.emit("message", "hello");
+    });
+
+    chat.on("error", error => {
+      console.log(error);
+    });
+
+    chat.on("close", () => {});
+  },
+  methods: {}
 };
 </script>
 
