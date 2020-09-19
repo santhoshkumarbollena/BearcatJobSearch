8b5faf07dacd7a1508c91ab4d05e26ef027c59dd
Anil Bomma <anil.bommaa@gmail.com>
2020-09-19 01:43:07 -0500

    added font-awsome icons

--- Code/bearcat-job-search-ui/src/main.js
+++ Code/bearcat-job-search-ui/src/main.js
@@ -6,11 +6,19 @@ import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import Argon from "./plugins/argon-kit";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+library.add(faSpinner);
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
