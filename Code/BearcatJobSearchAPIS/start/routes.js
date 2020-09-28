807042ad3e714a0123d74a14d779bb727d1b2129
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-09-28 11:35:37 -0500

    Added route

--- Code/BearcatJobSearchAPIS/start/routes.js
+++ Code/BearcatJobSearchAPIS/start/routes.js
@@ -39,6 +39,7 @@ Route.group(() => {
   Route.get("getStudent/:studentId", "StudentController.getStudentBasedOnId");
   Route.get("getAllJobs", "StudentController.getAllJobs");
   Route.get("getJob/:jobId", "StudentController.getJobBasedOnId");
+  Route.get("getJob/:jobId", "StudentController.getJobBasedOnId");
 }).prefix("api/v1/student");
 
 Route.group(() => {
