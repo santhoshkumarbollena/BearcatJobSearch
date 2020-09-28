397a99ef5c277ef5cc83fbdad112e1b544a1ae42
Anil Bomma <anil.bommaa@gmail.com>
2020-09-27 19:16:41 -0500

    added images

--- Code/bearcat-job-search-ui/src/views/Home.vue
+++ Code/bearcat-job-search-ui/src/views/Home.vue
@@ -2,39 +2,42 @@
   <div>
     <NavBar />
     <SubHeader :breadcrumb="breadcrumb" />
-    <b-row>
-      <b-col sm="8" md="8">
+    <div>
+      <ChatList />
+      <b-navbar-nav class="ml-auto">
         <b-nav-form>
           <b-form-input
-            class="mr-sm-2 ml-3"
+            class="mr-2 ml-3"
             placeholder="Search for jobs"
           ></b-form-input>
-          <b-button variant="info" class="my-2 my-sm-0" type="submit"
-            >Search</b-button
+          <b-button variant="info" class="my-2 my-sm-0" type="submit">
+            <i class="fa fa-search mr-1"></i>
+            Search</b-button
           >
         </b-nav-form>
-      </b-col>
-      <b-col sm="4" md="4">
-        <ChatList />
-      </b-col>
-    </b-row>
+      </b-navbar-nav>
+    </div>
 
     <div v-if="loader">
       <Loader v-if="loader"></Loader>
     </div>
     <div v-else>
       <hr class="mt-3 mb-3" />
-      <b-row>
-        <b-col sm="4" lg="4" v-for="job in jobs" :key="job.id">
-          <b-card :title="job.jobTitle" class="mb-2 card-bg">
-            <b-card-text>Description: {{ job.jobDescription }}</b-card-text>
-            <b-card-text>Employment Type: {{ job.employmentType }}</b-card-text>
-            <b-card-text>Salary: ${{ job.salary }}</b-card-text>
+      <b-container fluid>
+        <b-row>
+          <b-col sm="4" lg="4" v-for="job in jobs" :key="job.id">
+            <b-card :title="job.jobTitle" class="mb-2 card-bg">
+              <b-card-text>Description: {{ job.jobDescription }}</b-card-text>
+              <b-card-text
+                >Employment Type: {{ job.employmentType }}</b-card-text
+              >
+              <b-card-text>Salary: ${{ job.salary }}</b-card-text>
 
-            <b-button href="#" variant="primary">Apply</b-button>
-          </b-card>
-        </b-col>
-      </b-row>
+              <b-button href="#" variant="primary">Apply</b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
   </div>
 </template>
