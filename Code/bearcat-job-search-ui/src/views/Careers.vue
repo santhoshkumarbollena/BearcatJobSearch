c8ae122f45c04cc47484f8bdb90989d20fe25ebc
Santhosh Kumar Bollena <s537358@nwmissouri.edu>
2020-09-29 10:05:31 -0500

    Added the carres page

--- Code/bearcat-job-search-ui/src/views/Careers.vue
+++ Code/bearcat-job-search-ui/src/views/Careers.vue
@@ -23,42 +23,57 @@
     <b-container fluid>
       <b-row>
         <b-col sm="12" lg="12">
-          <h1>This is carrers page</h1>
+          <h1>Applied Jobs</h1>
         </b-col>
       </b-row>
     </b-container>
+    
+      <hr class="mt-3 mb-3" />
+      <b-container fluid>
+        <b-row>
+          <b-col sm="4" lg="4" v-for="job in jobs" :key="job.id">
+            <b-card
+              :title="job.jobTitle"
+              class="mb-2 m-auto card-1"
+              :img-src="require(`../assets/frontend.png`)"
+              img-alt="logo"
+              img-top
+              style="max-width: 22rem;"
+            >
+              <b-card-text class="mb-0">
+                Description:
+                <span class="fw-650 ml-2"> {{ job.jobDescription }}</span>
+              </b-card-text>
+              <b-card-text class="mb-0">
+                Employment-Type:
+                <span class="fw-650 ml-1">{{ job.employmentType }}</span>
+              </b-card-text>
+              <b-card-text>
+                Salary: <span class="fw-650 ml-1">${{ job.salary }}</span>
+              </b-card-text>
+
+              <b-card-text>
+                Status: <span class="fw-650 ml-1">Applied</span>
+              </b-card-text>
+
+              <b-button variant="info">
+                <i class="fa fa-info-circle"></i
+              ></b-button>
+
+              <b-button variant="danger" v-if="userRole == 'student'">
+                <i class="fa fa-check-square-o mr-1"></i>
+                cancel</b-button
+              >
+
+              <b-button variant="info" v-if="userRole == 'admin'">
+                <i class="fa fa-edit mr-1" /> Edit</b-button
+              >
+              <b-button variant="danger" v-if="userRole == 'admin'">
+                <i class="fa fa-trash mr-1"></i> Delete</b-button
+              >
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
   </div>
 </template>
-
-<script>
-import ChatList from "../components/Chat/ChatList";
-import SubHeader from "../components/Nav/SubHeader";
-import Loader from "../components/utils/Loader.vue";
-
-export default {
-  name: "Careers",
-  components: {
-    ChatList,
-    SubHeader,
-    Loader
-  },
-  data() {
-    return {
-      loader: true,
-      breadcrumb: [
-        {
-          text: "Careers",
-          href: "/careers"
-        }
-      ]
-    };
-  },
-  mounted() {
-    this.loader = false;
-  },
-  methods: {}
-};
-</script>
-
-<style scoped>
-</style>
