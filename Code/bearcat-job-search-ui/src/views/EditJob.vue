7419240a1c9d6d15f37007a3a60bfbc2eb80bebc
yugimamidi <yugi.mamidi01@gmail.com>
2020-09-30 21:03:28 -0500

    created UI for editing jobs

--- Code/bearcat-job-search-ui/src/views/EditJob.vue
+++ Code/bearcat-job-search-ui/src/views/EditJob.vue
@@ -1,35 +1,49 @@
 <template>
-  <!-- <div v-if="loader">
-    <Loader v-if="loader"></Loader>
-  </div>
-  <div v-else>
-    <SubHeader :breadcrumb="breadcrumb" />
-    <div>
-      <ChatList />
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input
-            class="mr-2 ml-5"
-            placeholder="Search for applied jobs"
-          ></b-form-input>
-          <b-button variant="info" class="my-2 my-sm-0" type="submit">
-            <i class="fa fa-search mr-1"></i>
-            Search</b-button
-          >
-        </b-nav-form>
-      </b-navbar-nav>
+ <div id="main-background">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 v-center p-4 ">
+          <h1 class="text-center">EDIT JOBS</h1>
+          <base-input
+            type="text"
+            placeholder="Job Description"
+            addon-left-icon="fa fa-file"
+            v-model="form.description"
+            required
+          ></base-input>
+           <small v-if="error.description" class="col-12 text-left text-danger">{{error.description}}</small>
+          <base-input
+            type="text"
+            placeholder="Employment Type"
+            addon-left-icon="fa fa-briefcase"
+            v-model="form.employment"
+            required
+          ></base-input>
+           <small v-if="error.employment" class="col-12 text-left text-danger">{{error.employment}}</small>
+           <base-input
+            type="number"
+            placeholder="Salary"
+            addon-left-icon="fa fa-money"
+            v-model="form.salary"
+            required
+          ></base-input>
+            <small v-if="error.salary" class="col-12 text-left text-danger">{{error.salary}}</small>
+          <base-button
+              type="primary"
+              class="btn pull-left mt-3 btn-icon btn-primary"
+              icon="fa fa-save"
+              @click.prevent.stop="save"
+            >Save</base-button>
+          <router-link to="/home">
+          <base-button
+              type="primary"
+              class="btn pull-right mt-3 btn-icon btn-primary"
+              icon="fa fa-ban"
+            >Cancel</base-button>
+          </router-link>
+        </div>
+      </div>
     </div>
-    <hr class="mt-3 mb-3" />
-    <b-container fluid>
-      <b-row>
-        <b-col sm="12" lg="12">
-          <h1>This is faq page</h1>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div> -->
-  <div>
-      <p>Editing Job</p>
   </div>
 </template>
 
@@ -39,28 +53,64 @@ import SubHeader from "../components/Nav/SubHeader";
 import Loader from "../components/utils/Loader.vue";
 
 export default {
-  name: "FAQ",
-  components: {
-    ChatList,
-    SubHeader,
-    Loader
-  },
   data() {
     return {
-      loader: true,
-      breadcrumb: [
-        {
-          text: "FAQ's",
-          href: "/faqs"
-        }
-      ]
+      error: {
+        description: "",
+        employment: "",
+        salary: ""
+      },
+      form: {
+        description: "",
+        employment: "",
+        salary: ""
+      }
     };
   },
   mounted() {
     this.loader = false;
-    console.log("In editing job job")
+    console.log("In editing job")
   },
-  methods: {}
+  methods: {
+     save() {
+      this.loader = true;
+      this.error = null;
+      this.error = {};
+
+      if (!this.form.description) {
+        console.log("Error for job description");
+        this.error.studentId = "please enter job description";
+      }
+      if (!this.form.employment) {
+        console.log("Error for employment type");
+        this.error.studentName = "please enter employment type";
+      }
+
+      if (!this.form.salary) {
+        console.log("Error for salary");
+        this.error.studentName = "please enter salary";
+      }
+
+      console.log(Object.keys(this.error).length);
+      if (Object.keys(this.error).length) {
+        console.log("In error");
+        return;
+      }
+
+      // this.$http
+      //   .post("save", this.form)
+      //   .then(response => {
+      //     this.endResult = "Saved successfully...!!";
+      //     alert("Saved succesfully");
+      //     window.location = "http://localhost:8080/#/home";
+      //   })
+      //   .catch(error => {
+      //     this.endResult = error.response
+      //       ? error.response.data.error.message
+      //       : error;
+      //   });
+    }
+  }
 };
 </script>
 
