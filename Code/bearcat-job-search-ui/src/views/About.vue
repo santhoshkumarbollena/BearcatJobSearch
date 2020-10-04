56d57dcb2e8269d56424b72fac8261284f1cba7c
yugimamidi <yugi.mamidi01@gmail.com>
2020-10-04 16:02:06 -0500

    added breadcrumb to about page

--- Code/bearcat-job-search-ui/src/views/About.vue
+++ Code/bearcat-job-search-ui/src/views/About.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
+     <SubHeader :breadcrumb="breadcrumb" />
     <h1>This is about page</h1>
   </div>
 </template>
 
 <script>
+import SubHeader from "../components/Nav/SubHeader";
 export default {
   name: "About",
+   components: {
+    SubHeader
+  },
   data() {
-    return {};
+    return {
+       breadcrumb: [
+        {
+          text: "About Us",
+          href: "#/about"
+        }
+      ]
+    };
   },
   mounted() {},
   methods: {}
