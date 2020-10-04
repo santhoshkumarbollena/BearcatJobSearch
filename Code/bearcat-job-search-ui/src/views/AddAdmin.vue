ce24445514e658338bc0e604e7f1a085f5df064a
yugimamidi <yugi.mamidi01@gmail.com>
2020-10-04 17:38:52 -0500

    created page for add admin

---
+++ Code/bearcat-job-search-ui/src/views/AddAdmin.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="main-background">
+    <SubHeader :breadcrumb="breadcrumb" />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 v-center">
+          <h1>ADD ADMIN</h1>
+          <base-input
+            type="text"
+            placeholder="User Id"
+            addon-left-icon="fa fa-id-badge"
+            v-model="form.userid"
+            required
+          ></base-input>
+          <small v-if="error.userid" class="col-12 text-left text-danger">{{
+            error.userid
+          }}</small>
+          <base-input
+            type="text"
+            placeholder="User Name"
+            addon-left-icon="fa fa-briefcase"
+            v-model="form.username"
+            required
+          ></base-input>
+          <small v-if="error.username" class="col-12 text-left text-danger">{{
+            error.username
+          }}</small>
+           <base-input
+            type="text"
+            placeholder="User Organization"
+            addon-left-icon="fa fa-money"
+            v-model="form.userOrganization"
+            required
+          ></base-input>
+          <small v-if="error.userOrganization" class="col-12 text-left text-danger">{{
+            error.userOrganization
+          }}</small>
+           <base-input
+            type="text"
+            placeholder="User Organization Description"
+            addon-left-icon="fa fa-money"
+            v-model="form.userOrganizationDescription"
+            required
+          ></base-input>
+          <small v-if="error.userOrganizationDescription" class="col-12 text-left text-danger">{{
+            error.userOrganizationDescription
+          }}</small>
+           <base-input
+            type="text"
+            placeholder="Phone Number"
+            addon-left-icon="fa fa-money"
+            v-model="form.phone"
+            required
+          ></base-input>
+          <small v-if="error.phone" class="col-12 text-left text-danger">{{
+            error.phone
+          }}</small>
+           <base-input
+            type="text"
+            placeholder="Email"
+            addon-left-icon="fa fa-money"
+            v-model="form.email"
+            required
+          ></base-input>
+          <small v-if="error.email" class="col-12 text-left text-danger">{{
+            error.email
+          }}</small>
+          <base-button
+            type="primary"
+            class="btn pull-left mt-3 btn-icon btn-primary"
+            icon="fa fa-save"
+            @click.prevent.stop="add"
+            >Add Admin</base-button
+          >
+          <router-link to="/home">
+            <base-button
+              type="danger"
+              class="btn pull-right mt-3 btn-icon btn-primary"
+              icon="fa fa-ban"
+              >Cancel</base-button
+            >
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatList from "../components/Chat/ChatList";
+import SubHeader from "../components/Nav/SubHeader";
+import Loader from "../components/utils/Loader.vue";
+
+export default {
+  name: "addadmin",
+  components: {
+    ChatList,
+    SubHeader,
+    Loader
+  },
+  data() {
+    return {
+      breadcrumb: [
+        {
+          text: "Jobs",
+          href: "#/home"
+        },
+        {
+          text: "Add Admin",
+          href: ""
+        }
+      ],
+      error: {
+        userid: "",
+        username: "",
+        userOrganization: "",
+        userOrganizationDescription: "",
+        phone: "",
+        email: ""
+      },
+      form: {
+        userid: "",
+        username: "",
+        userOrganization: "",
+        userOrganizationDescription: "",
+        phone: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+</style>
