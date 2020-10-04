e687e91dc9ef829f56e8f1df079c2c0ece7bc61b
yugimamidi <yugi.mamidi01@gmail.com>
2020-10-04 16:02:27 -0500

    added breadcrumb to edit faq page

---
+++ Code/bearcat-job-search-ui/src/views/EditFAQ.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <SubHeader :breadcrumb="breadcrumb" />
+    <h1>This is Edit Faq page</h1>
+  </div>
+</template>
+
+<script>
+import SubHeader from "../components/Nav/SubHeader";
+export default {
+  name: "EditFaq",
+   components: {
+    SubHeader
+  },
+  data() {
+    return {
+       breadcrumb: [
+        {
+          text: "FAQ's",
+          href: "#/faqs"
+        },
+        {
+          text: "Edit Faq",
+          href: ""
+        }
+      ]
+    };
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
