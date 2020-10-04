06eecbe49bdf69a7adb822f4d76c213cead20fea
MahenderReddy Surkanti <54380574+Mahender1166@users.noreply.github.com>
2020-10-04 18:21:40 -0500

    Update Home.vue

--- Code/bearcat-job-search-ui/src/views/Home.vue
+++ Code/bearcat-job-search-ui/src/views/Home.vue
@@ -7,19 +7,31 @@
       <SubHeader :breadcrumb="breadcrumb" />
       <div>
         <ChatList />
-         <router-link :to="{ name: 'add-admin' }">
-          <b-button variant="info" v-if="userRole == 'admin'" class="float-right mr-2">
+        <router-link :to="{ name: 'add-admin' }">
+          <b-button
+            variant="info"
+            v-if="userRole == 'admin'"
+            class="float-right mr-2"
+          >
             <i class="fa fa-user-secret mr-1" /> Add Admin
           </b-button>
         </router-link>
         <router-link :to="{ name: 'add-job' }">
-          <b-button variant="info" v-if="userRole == 'admin'" class="float-right mr-2">
+          <b-button
+            variant="info"
+            v-if="userRole == 'admin'"
+            class="float-right mr-2"
+          >
             <i class="fa fa-plus-square mr-1" /> Add Job
           </b-button>
         </router-link>
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input class="mr-2 ml-5" placeholder="Search for jobs" id="search"></b-form-input>
+            <b-form-input
+              class="mr-2 ml-5"
+              placeholder="Search for jobs"
+              id="search"
+            ></b-form-input>
             <b-button
               variant="info"
               class="my-2 my-sm-0"
@@ -31,7 +43,11 @@
               Search
             </b-button>
 
-            <b-button variant="info" class="my-2 my-sm-0" @click="clearSearch()">
+            <b-button
+              variant="info"
+              class="my-2 my-sm-0"
+              @click="clearSearch()"
+            >
               <i class="fa fa-times mr-1"></i>
               Clear
             </b-button>
@@ -42,7 +58,13 @@
       <hr class="mt-3 mb-3" />
       <b-container fluid>
         <b-row>
-          <b-col sm="4" lg="4" v-for="(job, index) in jobs" :key="job.id" class="mb-5">
+          <b-col
+            sm="4"
+            lg="4"
+            v-for="(job, index) in jobs"
+            :key="job.id"
+            class="mb-5"
+          >
             <b-card
               :title="job.jobTitle"
               class="mb-2 m-auto card-1"
@@ -68,21 +90,56 @@
                 <i class="fa fa-info-circle"></i>
               </b-button>
 
-              <b-button variant="info" v-if="userRole == 'student'" @click="applyJob(job)">
+              <b-button
+                variant="info"
+                v-if="userRole == 'student'"
+                @click="applyJob(job)"
+              >
                 <i class="fa fa-check-square-o mr-1"></i>
                 Apply
               </b-button>
 
-              <router-link :to="{ name: 'edit-job', params: { jobId: job.id } }">
+              <router-link
+                :to="{ name: 'edit-job', params: { jobId: job.id } }"
+              >
                 <b-button variant="info" v-if="userRole == 'admin'">
                   <i class="fa fa-edit mr-1" /> Edit
                 </b-button>
               </router-link>
-              <b-button variant="danger" v-if="userRole == 'admin'" class="ml-2">
+
+              <b-button
+                variant="danger"
+                v-if="userRole == 'admin'"
+                class="ml-2"
+                @click="showModal(job)"
+              >
                 <i class="fa fa-trash mr-1"></i> Delete
               </b-button>
             </b-card>
           </b-col>
+          <b-modal
+            id="delete-job"
+            ref="delete-job"
+            title="Delete Job"
+            @hidden="hideModal"
+            @cancel="hideModal"
+            @ok="confDeleteJob(deleteJob)"
+          >
+            <p class="my-4">
+              Are you sure you want to delete
+              <em class="font-weight-bold"> "{{ this.deleteJob.jobTitle }}"</em>
+              ?
+            </p>
+
+            <template v-slot:modal-footer="{ ok, cancel }">
+              <b-button variant="success" @click="ok()">
+                Yes
+              </b-button>
+              <b-button variant="danger" @click="cancel()">
+                Cancel
+              </b-button>
+            </template>
+          </b-modal>
         </b-row>
       </b-container>
     </div>
@@ -122,24 +179,59 @@ export default {
         }
       ],
       jobs: [],
-      error: null
+      error: null,
+      deleteJob: {}
     };
   },
   methods: {
+    showModal(job) {
+      this.deleteJob = job;
+      this.$refs["delete-job"].show();
+    },
+    hideModal() {
+      this.deleteJob = {};
+      this.$refs["delete-job"].hide();
+    },
+    confDeleteJob(job) {
+      this.$http
+        .delete("job/delete-job/" + job.id)
+        .then(response => {
+          this.$root.$bvToast.toast(
+            `${job.jobTitle} job deleted successfully`,
+            {
+              title: "Success",
+              autoHideDelay: 5000,
+              variant: "success"
+            }
+          );
+
+          this.$router.go();
+        })
+        .catch(error => {
+          this.endResult = error.response
+            ? error.response.data.error.message
+            : error;
+
+          this.$root.$bvToast.toast("error while updating the job", {
+            title: "Error",
+            autoHideDelay: 5000,
+            variant: "danger"
+          });
+        });
+
+      this.deleteJob = {};
+    },
     clearSearch() {
       document.getElementById("search").value = "";
       this.search = "";
       this.getJobs();
     },
     applyJob(job) {
-      console.log("Apply job" + job.id + " STudent id" + this.studentId);
-      //  http://127.0.0.1:3006/api/v1/studentApplication/StudentApplyJob/919583579/1
       this.$http
         .get(
           "studentApplication/StudentApplyJob/" + this.studentId + "/" + job.id
         )
         .then(response => {
-          console.log(response.data);
           this.loader = false;
         })
         .catch(error => {
@@ -150,7 +242,6 @@ export default {
         });
     },
     searchjob() {
-      console.log("In search");
       this.search = document.getElementById("search").value;
       this.getJobs();
     },
@@ -168,25 +259,6 @@ export default {
             : error;
         });
     }
-    // searchjob() {
-    //   this.search = document.getElementById("search").value;
-    //   this.$http
-    //     .get(
-    //       "job/searchJob" +
-    //         this.studentId +
-    //         "?search/" +
-    //         this.search
-    //     )
-    //     .then(response => {
-    //       this.jobs = response.data;
-    //       this.loader = false;
-    //     })
-    //     .catch(error => {
-    //       this.loader = false;
-    //       this.error = error.response ? error.response.data.error.message
-    //         : error;
-    //     });
-    // }
   },
   created() {
     this.$http
