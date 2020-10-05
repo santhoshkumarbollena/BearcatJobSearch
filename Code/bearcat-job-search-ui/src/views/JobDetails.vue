c9b9697f2250b44f090e1af48ccaf8b4cae49ecd
MahenderReddy Surkanti <54380574+Mahender1166@users.noreply.github.com>
2020-10-04 23:27:49 -0500

    Update JobDetails.vue

--- Code/bearcat-job-search-ui/src/views/JobDetails.vue
+++ Code/bearcat-job-search-ui/src/views/JobDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="main-background">
     <SubHeader :breadcrumb="breadcrumb" />
-    <div class="container">
+    <div class="pl-5 pr-5">
       <div class="row">
         <div class="col-md-12">
           <b-button
@@ -122,6 +122,107 @@
             readonly
           ></base-input>
         </div>
+
+        <div class="col-md-12" v-if="userRole == 'admin'">
+          <div class="mt-3 mb-2 border-bottom">
+            <h4>Applied Students</h4>
+          </div>
+          <div class="">
+            <el-table
+              class="table-responsive table-flush"
+              :data="appliedStudents"
+            >
+              <el-table-column>
+                <template v-slot="{ row }">
+                  <div class="media align-items-center">
+                    <a href="#" class="avatar rounded-circle mr-3">
+                      <img
+                        alt="Image placeholder"
+                        :src="require(`../assets/student.jpg`)"
+                      />
+                    </a>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column
+                label="Student ID"
+                min-width="200px"
+                prop="name"
+                sortable
+              >
+                <template v-slot="{ row }">
+                  <div class="media align-items-center">
+                    <div class="media-body mb-4 mt-4">
+                      <span class="font-weight-600 name mb-0 text-sm">
+                        {{ row.studentId }}
+                      </span>
+                    </div>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column
+                label="Student Name"
+                prop="budget"
+                min-width="140px"
+                sortable
+              >
+                <template v-slot="{ row }">
+                  <div class="media align-items-center">
+                    <div class="media-body">
+                      <span class="font-weight-600 name mb-0 text-sm">
+                        {{ row.studentName }}
+                      </span>
+                    </div>
+                  </div>
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                label="Contact Number"
+                prop="completion"
+                min-width="260px"
+                sortable
+              >
+                <template v-slot="{ row }">
+                  <div class="d-flex align-items-center">
+                    <span class="completion mr-2">{{ row.phoneNumber }}</span>
+                  </div>
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                label="Status"
+                prop="completion"
+                min-width="260px"
+                sortable
+              >
+                <template v-slot="{ row }">
+                  <div class="d-flex align-items-center">
+                    <span class="completion mr-2">{{ row.pivot.status }}</span>
+                  </div>
+                </template>
+              </el-table-column>
+
+              <el-table-column label="Action" min-width="260px" sortable>
+                <template v-slot="{ row }">
+                  <div class="d-flex align-items-center">
+                    <router-link
+                      :to="{
+                        name: 'student-info',
+                        params: { studentId: row.studentId }
+                      }"
+                    >
+                      <base-button type="primary" size="sm">Info</base-button>
+                    </router-link>
+                    <base-button type="success" class="ml-1" size="sm">Approve</base-button>
+                    <base-button type="danger" size="sm">Decline</base-button>
+
+                  </div>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -131,18 +232,31 @@
 import ChatList from "../components/Chat/ChatList";
 import SubHeader from "../components/Nav/SubHeader";
 import Loader from "../components/utils/Loader.vue";
+import {
+  Table,
+  TableColumn,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown
+} from "element-ui";
 
 export default {
   name: "editjob",
   components: {
     ChatList,
     SubHeader,
-    Loader
+    Loader,
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+    [Dropdown.name]: Dropdown,
+    [DropdownItem.name]: DropdownItem,
+    [DropdownMenu.name]: DropdownMenu
   },
   data() {
     return {
       userRole: localStorage.getItem("role"),
       jobs: {},
+      appliedStudents: [],
       breadcrumb: [
         {
           text: "Jobs",
@@ -171,6 +285,21 @@ export default {
       .then(response => {
         if (response.data && response.data.length) {
           this.form = response.data[0];
+          this.loader = false;
+        }
+      })
+      .catch(error => {
+        this.endResult = error.response
+          ? error.response.data.error.message
+          : error;
+      });
+
+    this.$http
+      .get("studentApplication/get/student/applied/jobs/" + path_id)
+      .then(response => {
+        if (response.data && response.data.length) {
+          this.appliedStudents = response.data[0].jobApplications;
+          console.log(this.appliedStudents);
           this.loader = false;
         }
       })
