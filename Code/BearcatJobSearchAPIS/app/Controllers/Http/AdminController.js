84980d6073d3f1bd3bd6107a496a790bceb00376
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-10-04 20:34:35 -0500

    Updating the admin controller

--- Code/BearcatJobSearchAPIS/app/Controllers/Http/AdminController.js
+++ Code/BearcatJobSearchAPIS/app/Controllers/Http/AdminController.js
@@ -1,3 +1,5 @@
+
+
 "use strict";
 
 const _ = use("lodash");
@@ -6,11 +8,14 @@ const Student = use("App/Models/Student");
 const User = use("App/Models/User")
 
 class AdminController {
-   async createAdmin({ request, auth, response, params }) {
+
+    async createAdmin({ request, auth, response, params }) {
         let userInput = request.all();
-     let student={};
-     userInput.userId = Math.floor(Math.random() * 1000000000);
-     student.studentId= userInput.userId;
+        console.log("In admin creation userInput",userInput)
+        let student={};
+        userInput.userId = Math.floor(Math.random() * 1000000000);
+        console.log(userInput)
+        student.studentId= userInput.userId;
         
         student.sudentName = userInput.userName;
         student.email = userInput.email;
@@ -19,11 +24,16 @@ class AdminController {
         student.gender = userInput.gender;
         student.phoneNumber = userInput.phoneNumber
         student.role = "admin";
-      userInput = _.omit(userInput, ["gender","dob","password"]);
-       const user = await User.findOrCreate(userInput);
-     
+
+        console.log("student",student)
+        userInput = _.omit(userInput, ["gender","dob","password"]);
+        console.log("userInput",userInput)
+
+        const user = await User.findOrCreate(userInput);
         return response.status(200).json(user);
     }
+
+   
 
 }
 module.exports = AdminController;
