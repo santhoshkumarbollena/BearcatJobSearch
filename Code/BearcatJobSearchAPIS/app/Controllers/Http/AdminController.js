7c2bd81bbc8ebee877d04e5e9f155a690aaa10d6
saichandugampa <54381036+saichandugampa@users.noreply.github.com>
2020-10-04 20:30:19 -0500

    Importing required models

--- Code/BearcatJobSearchAPIS/app/Controllers/Http/AdminController.js
+++ Code/BearcatJobSearchAPIS/app/Controllers/Http/AdminController.js
@@ -1,6 +1,9 @@
 "use strict";
 
-
+const _ = use("lodash");
+const Hash = use("Hash");
+const Student = use("App/Models/Student");
+const User = use("App/Models/User")
 
 class AdminController {
    
