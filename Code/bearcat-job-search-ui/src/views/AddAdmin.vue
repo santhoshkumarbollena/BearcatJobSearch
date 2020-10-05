70239350d62e832ec916bbfc548eb3542c685ecf
saichandugampa <54381036+saichandugampa@users.noreply.github.com>
2020-10-05 00:27:41 -0500

    Update AddAdmin.vue

--- Code/bearcat-job-search-ui/src/views/AddAdmin.vue
+++ Code/bearcat-job-search-ui/src/views/AddAdmin.vue
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="col-md-6 v-center">
           <h1>ADD ADMIN</h1>
-         
+
           <base-input
             type="text"
             placeholder="User Name"
@@ -16,27 +16,31 @@
           <small v-if="error.username" class="col-12 text-left text-danger">{{
             error.username
           }}</small>
-           <base-input
+          <base-input
             type="text"
             placeholder="User Organization"
             addon-left-icon="fa fa-money"
             v-model="form.userOrganization"
             required
           ></base-input>
-          <small v-if="error.userOrganization" class="col-12 text-left text-danger">{{
-            error.userOrganization
-          }}</small>
-           <base-input
+          <small
+            v-if="error.userOrganization"
+            class="col-12 text-left text-danger"
+            >{{ error.userOrganization }}</small
+          >
+          <base-input
             type="text"
             placeholder="User Organization Description"
             addon-left-icon="fa fa-money"
             v-model="form.userOrganizationDescription"
             required
           ></base-input>
-          <small v-if="error.userOrganizationDescription" class="col-12 text-left text-danger">{{
-            error.userOrganizationDescription
-          }}</small>
-           <base-input
+          <small
+            v-if="error.userOrganizationDescription"
+            class="col-12 text-left text-danger"
+            >{{ error.userOrganizationDescription }}</small
+          >
+          <base-input
             type="text"
             placeholder="Phone Number"
             addon-left-icon="fa fa-money"
@@ -46,7 +50,7 @@
           <small v-if="error.phone" class="col-12 text-left text-danger">{{
             error.phone
           }}</small>
-           <base-input
+          <base-input
             type="text"
             placeholder="Email"
             addon-left-icon="fa fa-money"
@@ -57,27 +61,29 @@
             error.email
           }}</small>
           <base-input
-            type="text"
+            type="password"
             placeholder="Password"
             addon-left-icon="fa fa-money"
             v-model="form.password"
             required
           ></base-input>
-          <small v-if="error.email" class="col-12 text-left text-danger">{{
+          <small v-if="error.password" class="col-12 text-left text-danger">{{
             error.email
           }}</small>
-           <base-input addon-left-icon="fa fa-calendar">
+          <base-input addon-left-icon="fa fa-calendar">
             <flat-picker
-              slot-scope="{focus, blur}"
+              slot-scope="{ focus, blur }"
               @on-open="focus"
               @on-close="blur"
-              :config="{allowInput: true}"
+              :config="{ allowInput: true }"
               class="form-control datepicker"
               v-model="form.dob"
               placeholder="DOB"
             ></flat-picker>
           </base-input>
-          <small v-if="error.dob" class="col-12 text-left text-danger">{{error.dob}}</small>
+          <small v-if="error.dob" class="col-12 text-left text-danger">{{
+            error.dob
+          }}</small>
           <b-form-radio-group
             v-model="form.gender"
             :options="options"
@@ -146,34 +152,72 @@ export default {
         { item: "Male", name: "Male" },
         { item: "Female", name: "Female" }
       ],
-      form: {
-        
-      }
+      form: {}
     };
   },
   methods: {
-    add(){
-      console.log("Add admin"+this.form)
-      this.form.role="admin"
-      this.$http
-      .post("admin/create-admin",this.form)
-      .then(response => {
-        console.log(response.data)
-        this.$root.$bvToast.toast(
-            `Admin added sucessfully`,
-            {
-              title: "Success",
-              autoHideDelay: 5000,
-              variant: "success"
-            }
-          );
-        
-      })
-      .catch(error => {
-        console.log("error")
-      });
-    }
+    add() {
+      this.error = {};
+      if (!this.form.userName) {
+        console.log("Error for userName:");
+        this.error.username = "please enter user name";
+      }
+      if (!this.form.userOrganization) {
+        console.log("Error for userName:");
+        this.error.userOrganization = "please enter user organization";
+      }
 
+      if (!this.form.userOrganizationDescription) {
+        console.log("Error for userName:");
+        this.error.userOrganizationDescription =
+          "please enter user organization description";
+      }
+
+      if (!this.form.phoneNumber) {
+        console.log("Error for phoneNumber");
+        this.error.phoneNumber = "please enter phone number";
+      }
+
+      if (!this.form.email) {
+        console.log("Error for phoneNumber");
+        this.error.email = "please enter email";
+      }
+
+      if (!this.form.password) {
+        console.log("Error for phoneNumber");
+        this.error.password = "please enter password";
+      }
+
+      if (!this.form.dob) {
+        console.log("Error for dob");
+        this.error.dob = "please enter dob";
+      }
+
+      if (!this.form.gender) {
+        console.log("Error for dob");
+        this.error.gender = "please enter gender";
+      }
+
+      if (Object.keys(this.error).length > 0) {
+        return;
+      }
+
+      this.form.role = "admin";
+      this.$http
+        .post("admin/create-admin", this.form)
+        .then(response => {
+          console.log(response.data);
+          this.$router.go(-1);
+          this.$root.$bvToast.toast(`Admin added sucessfully`, {
+            title: "Success",
+            autoHideDelay: 5000,
+            variant: "success"
+          });
+        })
+        .catch(error => {
+          console.log("error");
+        });
+    }
   }
 };
 </script>
