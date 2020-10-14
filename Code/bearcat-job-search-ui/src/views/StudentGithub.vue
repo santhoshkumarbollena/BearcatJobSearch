6b6c6eed8f6b0f29a61cd10074faa1f8d9f2d5a5
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-10-13 22:36:43 -0500

    Basic github repos functionality

---
+++ Code/bearcat-job-search-ui/src/views/StudentGithub.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+      <div class="mt-2">
+   
+  </div>
+  </div>
+</template>
+
+
+
+<script>
+import ChatList from "../components/Chat/ChatList";
+import SubHeader from "../components/Nav/SubHeader";
+import Loader from "../components/utils/Loader.vue";
+import {
+  Table,
+  TableColumn,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown
+} from "element-ui";
+
+export default {
+  name: "Students",
+  components: {
+    ChatList,
+    SubHeader,
+    Loader,
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+    [Dropdown.name]: Dropdown,
+    [DropdownItem.name]: DropdownItem,
+    [DropdownMenu.name]: DropdownMenu
+  },
+  data() {
+    return {
+      loader: true,
+      userRole: localStorage.getItem("role"),
+      search: "",
+      githubRepos:[],
+      userName:"santhoshkumarbollena",
+      students: [],
+      breadcrumb: [
+        {
+          text: "Students",
+          href: "#/students"
+        },
+        {
+          text: "Student Info",
+          href: ""
+        }
+      ],
+      form: {
+        studentId: "",
+        studentName: ""
+      },
+      currentPage: 1
+    };
+  },
+  mounted() {
+     
+   
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
