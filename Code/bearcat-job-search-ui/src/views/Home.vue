d6aff378a1efeccea04bea73ee0aff412a7ca415
Anil Bomma <anil.bommaa@gmail.com>
2020-10-25 20:45:49 -0500

    updated home page

--- Code/bearcat-job-search-ui/src/views/Home.vue
+++ Code/bearcat-job-search-ui/src/views/Home.vue
@@ -8,18 +8,30 @@
       <div>
         <ChatList />
         <router-link :to="{ name: 'add-admin' }">
-          <b-button variant="info" v-if="userRole == 'admin'" class="float-right mr-2">
+          <b-button
+            variant="info"
+            v-if="userRole == 'admin'"
+            class="float-right mr-2"
+          >
             <i class="fa fa-user-secret mr-1" /> Add Admin
           </b-button>
         </router-link>
         <router-link :to="{ name: 'add-job' }">
-          <b-button variant="info" v-if="userRole == 'admin'" class="float-right mr-2">
+          <b-button
+            variant="info"
+            v-if="userRole == 'admin'"
+            class="float-right mr-2"
+          >
             <i class="fa fa-plus-square mr-1" /> Add Job
           </b-button>
         </router-link>
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input class="mr-2 ml-5" placeholder="Search for jobs" id="search"></b-form-input>
+            <b-form-input
+              class="mr-2 ml-5"
+              placeholder="Search for jobs"
+              id="search"
+            ></b-form-input>
             <b-button
               variant="info"
               class="my-2 my-sm-0"
@@ -31,7 +43,11 @@
               Search
             </b-button>
 
-            <b-button variant="info" class="my-2 my-sm-0" @click="clearSearch()">
+            <b-button
+              variant="info"
+              class="my-2 my-sm-0"
+              @click="clearSearch()"
+            >
               <i class="fa fa-times mr-1"></i>
               Clear
             </b-button>
@@ -45,7 +61,13 @@
           <!-- <b-col sm="12">
             <iframe width="560" height="315" src="https://www.linkedin.com/" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </b-col> -->
-          <b-col sm="4" lg="4" v-for="(job, index) in jobs" :key="job.id" class="mb-5">
+          <b-col
+            sm="4"
+            lg="4"
+            v-for="(job, index) in jobs"
+            :key="job.id"
+            class="mb-5"
+          >
             <b-card
               :title="job.jobTitle"
               class="mb-2 m-auto card-1"
@@ -67,18 +89,26 @@
                 <span class="fw-650 ml-1">${{ job.salary }}</span>
               </b-card-text>
 
-              <router-link :to="{ name: 'job-details', params: { jobId: job.id } }">
+              <router-link
+                :to="{ name: 'job-details', params: { jobId: job.id } }"
+              >
                 <b-button variant="info" class="mr-2">
                   <i class="fa fa-info-circle"></i>
                 </b-button>
               </router-link>
 
-              <b-button variant="info" v-if="userRole == 'student'" @click="applyJob(job)">
-                <i class="fa fa-check-square-o mr-1"></i>
-                Apply
-              </b-button>
+              <router-link
+                :to="{ name: 'apply-job', params: { jobId: job.id } }"
+              >
+                <b-button variant="info" v-if="userRole == 'student'">
+                  <i class="fa fa-check-square-o mr-1"></i>
+                  Apply
+                </b-button>
+              </router-link>
 
-              <router-link :to="{ name: 'edit-job', params: { jobId: job.id } }">
+              <router-link
+                :to="{ name: 'edit-job', params: { jobId: job.id } }"
+              >
                 <b-button variant="info" v-if="userRole == 'admin'">
                   <i class="fa fa-edit mr-1" /> Edit
                 </b-button>
@@ -104,9 +134,7 @@
           >
             <p class="my-4">
               Are you sure you want to delete
-              <em
-                class="font-weight-bold"
-              >"{{ this.deleteJob.jobTitle }}"</em>
+              <em class="font-weight-bold">"{{ this.deleteJob.jobTitle }}"</em>
               ?
             </p>
 
@@ -117,7 +145,9 @@
           </b-modal>
 
           <b-col sm="12" md="12" v-if="jobs.length === 0">
-            <div class="alert alert-warning" role="alert">No jobs have posted yet...!!!</div>
+            <div class="alert alert-warning" role="alert">
+              No jobs have posted yet...!!!
+            </div>
           </b-col>
         </b-row>
       </b-container>
@@ -235,6 +265,7 @@ export default {
           });
         });
     },
+    
     searchjob() {
       this.search = document.getElementById("search").value;
       this.getJobs();
