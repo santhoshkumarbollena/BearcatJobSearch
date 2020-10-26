0072a0d16f7941d0e00c5335dea8b234ab475fc5
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-10-25 21:50:35 -0500

    Updating the student application table to store the applied job data

--- Code/BearcatJobSearchAPIS/database/migrations/1592959489692_student_applications_schema.js
+++ Code/BearcatJobSearchAPIS/database/migrations/1592959489692_student_applications_schema.js
@@ -14,6 +14,25 @@ class StudentApplicationsSchema extends Schema {
       table.integer("jobId").unsigned();
       table.foreign("jobId").references("jobs.id").onDelete("cascade");
       table.string("status", 20).notNullable();
+      table.string("studentName", 40).notNullable();
+      table.string("email", 40).notNullable();
+      table.timestamp("dob").notNullable();
+      table.string("phoneNumber", 20).notNullable();
+      table.string("gender", 10).notNullable();
+      table.string("ugUniversity", 40);
+      table.string("ugDegree", 40);
+      table.string("ugDepartment", 40);
+      table.string("ugScore", 10);
+      table.string("graduateUniversity", 40);
+      table.string("graduateDegree", 40);
+      table.string("graduateDepartment", 40);
+      table.string("graduateScore", 40);
+      table.integer("experienceYears");
+      table.integer("expectedSalary");
+      table.string("employementType", 10);
+      table.string("gitHubUrl", 100);
+      table.string("linkedInUrl", 100);
+      table.string("role", 20);
       table.timestamps();
     });
   }
